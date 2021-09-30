0833c5adfda150ba40ccb721284ac5b5f2527696
Matthias Hryniszak <mhryn@softserveinc.com>
2021-09-30 11:42:10 +0200

    Extended the file example to decode all the messages

--- examples/send-receive-file.ts
+++ examples/send-receive-file.ts
@@ -2,6 +2,7 @@
 
 import { createReadStream } from 'fs'
 import { MavLinkPacketSplitter, MavLinkPacketParser, MavLinkPacket } from '..'
+import { minimal, common, ardupilotmega, uavionix, icarous } from '..'
 
 const file = createReadStream('./mavlink-v2-3412-packets.bin')
 
@@ -11,8 +12,22 @@ const reader = file
   .pipe(splitter)
   .pipe(new MavLinkPacketParser())
 
-reader.on('data', (packet: MavLinkPacket) => {
-  console.log(packet.debug())
+
+// create a registry of mappings between a message id and a data class
+const REGISTRY = {
+  ...minimal.REGISTRY,
+  ...common.REGISTRY,
+  ...ardupilotmega.REGISTRY,
+  ...uavionix.REGISTRY,
+  ...icarous.REGISTRY,
+}
+
+reader.on('data', packet => {
+  const clazz = REGISTRY[packet.header.msgid]
+  if (clazz) {
+    const data = packet.protocol.data(packet.payload, clazz)
+    console.log(data)
+  }
 })
 
 file.on('close', () => {
