5bc9eda2aca8a3dd55cd779f62cb303e24061bad
Matthias Hryniszak <padcom@gmail.com>
2022-08-25 14:01:23 +0200

    Made the sanity checks independent of the current folder

--- sanity-check.cjs
+++ sanity-check.cjs
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
+const { join } = require('node:path')
 const { createReadStream } = require('node:fs')
 const { createMavLinkStream } = require('.')
 
-const port = createReadStream('examples/GH-5.bin')
+const port = createReadStream(join(__dirname, 'examples/GH-5.bin'))
 const parser = createMavLinkStream(port)
 parser.on('data', packet => console.log(packet.debug()))
--- sanity-check.mjs
+++ sanity-check.mjs
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
+import { join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { createReadStream } from 'node:fs'
 import { createMavLinkStream } from './dist/index.js'
 
-const port = createReadStream('./examples/GH-5.bin')
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const port = createReadStream(join(__dirname, 'examples/GH-5.bin'))
 const parser = createMavLinkStream(port)
 parser.on('data', packet => console.log(packet.debug()))
