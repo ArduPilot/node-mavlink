3a2a5f8113772f783c4cbfe80715145dc855180d
Matthias Hryniszak <padcom@gmail.com>
2022-08-25 16:07:15 +0200

    Added plain javascript sanity check (commonjs)

---
+++ sanity-check.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const { join } = require('node:path')
+const { createReadStream } = require('node:fs')
+const { createMavLinkStream } = require('./dist')
+
+const port = createReadStream(join(__dirname, 'examples/GH-5.bin'))
+const parser = createMavLinkStream(port)
+parser.on('data', packet => console.log(packet.debug()))
